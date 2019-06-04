4f7056047744bdccbfa03a5780a9b13779433f2f
Tyler McWilliam <tylermcwilliam@gmail.com>
2019-06-04 18:42:51 +0800

    untested changes

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,10 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:show, :edit]
+  before_action :set_polls, only: [:show]
+  before_action :set_orders, only: [:show]
+  before_action :set_positions, only: [:show]
+  before_action :set_users, only: [:show]
+
   def index
     @groups = Group.all
   end
@@ -16,6 +21,36 @@ class GroupsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  def set_polls
+    @polls = Poll.select do |poll|
+      poll.group_id == @group.id
+    end
+  end
+
+  def set_orders
+    @orders = Order.select do |order|
+      order.group_id == @group.id
+    end
+  end
+
+  def set_positions
+    @positions = Position.select do |position|
+      position.group_id == @group.id
+    end
+  end
+
+  def set_users
+    @members = User.select do |user|
+      user.groups.contains(@group.id)
+    end
   end
 
   def group_params
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,15 @@
 class VotesController < ApplicationController
+
+  def new
+    @vote = Vote.new
+  end
+
   def create
     @vote = Vote.new
+    @poll = Poll.find(params[:poll_id])
+    @group = @poll.group
+    @vote.voting_power = vote.user.user_groups.find(@group.id).user_share / @group.total_shares
+    @vote.poll_id = @poll.id
+    @poll.approval += @vote.voting_power if @vote.approve == true
   end
 end
