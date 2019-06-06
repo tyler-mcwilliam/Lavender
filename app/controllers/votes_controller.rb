4ab60ea091cdbccae484c3698c19bfa64c70f2f7
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-06-06 13:06:44 +0800

    a lot of voting direction

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,15 +1,21 @@
 class VotesController < ApplicationController
-
   def new
     @vote = Vote.new
   end
 
   def create
     @vote = Vote.new
+    @vote.approve = true if params[:commit] == 'yes'
+    @vote.approve = false if params[:commit] == 'no'
+
     @poll = Poll.find(params[:poll_id])
     @group = @poll.group
-    @vote.voting_power = vote.user.user_groups.find(@group.id).user_share / @group.total_shares
+    @vote.voting_power = current_user.user_groups.find(@group.id).user_share / @group.total_shares
     @vote.poll_id = @poll.id
+
     @poll.approval += @vote.voting_power if @vote.approve == true
+    @vote.save
+    @poll.approval += @vote.voting_power if @vote.approve == true
+    redirect_to group_path(@group)
   end
 end
