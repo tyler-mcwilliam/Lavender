2ac677d4ce658e1ec5df49308dafb87eee7f11d3
Tony Yu <tonyyu1129@hotmail.com>
2019-06-07 07:59:43 +0800

    tried js, didn't work T_T

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,7 @@
 import "bootstrap";
 import "mainchart";
+
+$(document).on("click", "#join-group-button", function () {
+     const groupId = $(this).data('id');
+     $(".modal-body #user_group_group_id").val( groupId );
+});
