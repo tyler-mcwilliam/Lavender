78d258fc5c7924e2edcca11e2e9395908230eb5b
Tyler McWilliam <tylermcwilliam@gmail.com>
2019-06-07 16:45:02 +0800

    stuff

--- app/controllers/user_groups_controller.rb
+++ app/controllers/user_groups_controller.rb
@@ -11,10 +11,7 @@ class UserGroupsController < ApplicationController
     @user_group.user_share += params[:user_group][:initial_deposit].to_f / (@group.portfolio_value / @group.total_shares)
     @group.total_shares += @user_group.user_share
     current_user.available_balance -= params[:user_group][:initial_deposit].to_f
-    @grouptal_shares -= cancelledshares
-    @user_group.user_contribution -= withdrawal
-    @user_group.user_share -= cancelledshares
-    @user_g.cash_value += params[:user_group][:initial_deposit].to_f
+    @group.cash_value += params[:user_group][:initial_deposit].to_f
     if @user_group.save!
       @group.save
       current_user.save
@@ -30,6 +27,40 @@ class UserGroupsController < ApplicationController
     @user_group.deposit(params[:user_group][:deposit]) unless params[:user_group][:deposit].nil?
     @user_group.withdraw(params[:user_group][:withdrawal]) unless params[:user_group][:withdrawal].nil?
     @user_group.initial_deposit(params[:initial_deposit]) unless params[:initial_deposit].nil?
+    @user_group.save
+  end
+
+  def initial_deposit(deposit)
+    @user_group.user = current_user
+    @group = Group.find(params[:group_id])
+    newshares = deposit * 100
+    @group.cash_value += deposit
+    @group.total_shares += newshares
+    @user_group.user_contribution += deposit
+    @user_group.user_share += newshares
+    @user_group.user_balance = @group.investment_value * (@user_group.user_share / @group.total_shares)
+    @user_group.save
+  end
+
+  def deposit(deposit)
+    @group = Group.find(params[:group_id])
+    newshares = deposit / (@group.investment_value / @group.total_shares)
+    @group.cash_value += deposit
+    @group.total_shares += newshares
+    @user_group.user_contribution += deposit
+    @user_group.user_share += newshares
+    @user_group.user_balance = @group.investment_value * (@user_group.user_share / @group.total_shares)
+    @user_group.save
+  end
+
+  def withdraw(withdrawal)
+    @group = self.group
+    cancelledshares = withdrawal / (@group.investment_value / @group.total_shares)
+    @group.cash_value -= withdrawal
+    @group.total_shares -= cancelledshares
+    @user_group.user_contribution -= withdrawal
+    @user_group.user_share -= cancelledshares
+    @user_group.user_balance = @group.investment_value * (@user_group.user_share / @group.total_shares)
     @user_group.save
   end
 
--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -2,39 +2,5 @@ class UserGroup < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
-  def initial_deposit(deposit)
-    @user_group.user = current_user
-    @group = Group.find(params[:group_id])
-    newshares = deposit * 100
-    @group.cash_value += deposit
-    @group.total_shares += newshares
-    @user_group.user_contribution += deposit
-    @user_group.user_share += newshares
-    @user_group.user_balance = @group.investment_value * (@user_group.user_share / @group.total_shares)
-    @user_group.save
-  end
-
-  def deposit(deposit)
-    @group = Group.find(params[:group_id])
-    newshares = deposit / (@group.investment_value / @group.total_shares)
-    @group.cash_value += deposit
-    @group.total_shares += newshares
-    @user_group.user_contribution += deposit
-    @user_group.user_share += newshares
-    @user_group.user_balance = @group.investment_value * (@user_group.user_share / @group.total_shares)
-    @user_group.save
-  end
-
-  def withdraw(withdrawal)
-    @group = self.group
-    cancelledshares = withdrawal / (@group.investment_value / @group.total_shares)
-    @group.cash_value -= withdrawal
-    @group.total_shares -= cancelledshares
-    @user_group.user_contribution -= withdrawal
-    @user_group.user_share -= cancelledshares
-    @user_group.user_balance = @group.investment_value * (@user_group.user_share / @group.total_shares)
-    @user_group.save
-  end
-
   attr_accessor :initial_deposit, :deposit, :withdrawal
 end
