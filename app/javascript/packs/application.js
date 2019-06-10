d1fc3cb6e6157840d60d6345bcb8ae18b922d3b2
Michael Lauritsch <ML62686@gmail.com>
2019-06-10 16:36:29 +0800

    Fixed accidental application.js delete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,7 @@ import "bootstrap";
 import "chatroom";
 
 import { mainChart } from "components/mainchart";
+const groupShow = document.querySelector('groups show');  
+if (groupShow != null) {  
+  mainChart();  
+}
