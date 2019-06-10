77cc19088fc5fc5ed396fb79ff7c7e2f32e54cf2
Tony Yu <tonyyu1129@hotmail.com>
2019-06-10 22:22:37 +0800

    updated formulas in user.rb with cents and cents()

--- app/controllers/user_groups_controller.rb
+++ app/controllers/user_groups_controller.rb
@@ -5,13 +5,13 @@ class UserGroupsController < ApplicationController
 
   def create
     @user_group = UserGroup.new(user_group_params)
-    @user_group.user_contribution += params[:user_group][:initial_deposit].to_f
+    @user_group.user_contribution_cents += cents([:user_group][:initial_deposit])
     @user_group.user_id = current_user.id
     @group = @user_group.group
-    @user_group.user_share += params[:user_group][:initial_deposit].to_f / (@group.portfolio_value / @group.total_shares)
+    @user_group.user_share += cents(params[:user_group][:initial_deposit]) / (@group.portfolio_value_cents / @group.total_shares)
     @group.total_shares += @user_group.user_share
-    current_user.available_balance -= params[:user_group][:initial_deposit].to_f
-    @group.cash_value += params[:user_group][:initial_deposit].to_f
+    current_user.available_balance_cents -= cents(params[:user_group][:initial_deposit])
+    @group.cash_value_cents += cents(params[:user_group][:initial_deposit])
     if @user_group.save!
       @group.save
       current_user.save
@@ -23,9 +23,9 @@ class UserGroupsController < ApplicationController
 
   def update
     @user_group = UserGroup.find(params[:user_group][:user_group_id])
-    @user_group.deposits(params[:user_group][:deposit].to_f) unless params[:user_group][:deposit].nil?
-    @user_group.withdraw(params[:user_group][:withdrawal].to_f) unless params[:user_group][:withdrawal].nil?
-    @user_group.first_deposit(params[:initial_deposit].to_f) unless params[:initial_deposit].nil?
+    @user_group.deposits(cents(params[:user_group][:deposit])) unless params[:user_group][:deposit].nil?
+    @user_group.withdraw(cents(params[:user_group][:withdrawal])) unless params[:user_group][:withdrawal].nil?
+    @user_group.first_deposit(cents(params[:initial_deposit])) unless params[:initial_deposit].nil?
     @user_group.save!
     redirect_to dashboard_path
   end
