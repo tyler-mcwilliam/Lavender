d9fc57bf3f7756d6a194cfb461961616576b1be3
tyler-mcwilliam <45588507+tyler-mcwilliam@users.noreply.github.com>
2019-07-15 13:33:51 -0700

    Update poll.rb

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -12,6 +12,12 @@ class Poll < ApplicationRecord
   monetize :stop_loss_price_cents, allow_nil: true
   monetize :price_cents
 
+  before_action :set_stock_quote
+  
+  def set_stock_quote
+    StockQuote::Stock.new(api_key: 'pk_5f4dbf25fbd3494cbbd71fe4c33393fe')
+  end
+  
   # return true or false if poll has votes from current user
   def has_voted_by_current_user(current_user)
     # raise
